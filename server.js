21b66025b401b00de6c996c135c8d2bf19d6b2e0
anjali-yadav17 <anjaliy523281@gmail.com>
2025-08-28 19:06:27 +0530

    updated file

--- server.js
+++ server.js
@@ -79,7 +79,7 @@ app.post('/api/admin/login', async (req, res) => {
   }
 });
 
-// Donor Routes
+// Donor Route
 app.post('/api/donors', async (req, res) => {
   try {
     const donor = new Donor(req.body);
